ef1a9938ad1fbe75534ff2f46bb8164abd7630d0
googol88 <ronaldfleung@gmail.com>
2021-11-24 20:54:35 -0500

    🚧 dropdowns are hard

--- components/nav-dropdown.js
+++ components/nav-dropdown.js
@@ -1,4 +1,4 @@
-import { Flex, Box, NavLink, Text } from 'theme-ui';
+import { Box, NavLink } from 'theme-ui';
 import { ChevronDown } from 'react-feather';
 
 const NavDropdown = ({ label, ...props }) => {
@@ -6,7 +6,18 @@ const NavDropdown = ({ label, ...props }) => {
     <>
       <NavLink p={2} sx={{cursor: "pointer"}}>
         Previous Years{" "}<ChevronDown style={{verticalAlign: "middle"}} size={16} />
-        <Box as="nav" sx={{display: "flex", flexDirection: "column", position: "absolute", transformOrigin: "top-right", right: 0}}>
+        <Box
+          sx={{
+            display: "none",
+            flexDirection: "column",
+            position: "absolute",
+            transformOrigin: "top-right",
+            right: 0,
+            ':hover,:focus': {
+              display: "flex"
+            }
+          }}
+        >
           {props.children}
         </Box>
       </NavLink>
