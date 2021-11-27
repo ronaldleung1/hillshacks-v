d8691598ec36b8c6ffe381a53ce8b2211afa8946
googol88 <ronaldfleung@gmail.com>
2021-11-26 21:22:49 -0500

    ✨ feat: overengineered navbar

--- components/nav.js
+++ components/nav.js
@@ -2,33 +2,54 @@ import { Flex, Box, NavLink } from 'theme-ui';
 import ColorSwitcher from '../components/color-switcher';
 import NavDropdown from '../components/nav-dropdown';
 
-const Nav = (props) => {
+const Nav = props => {
   return (
-    <Flex as="nav">
-      <Box sx={{flex: '1 1 auto', margin: '2'}}>
-        <NavLink href="#!" p={2}>
-          Home
-        </NavLink>
-        <NavLink href="#!" p={2}>
-          About
-        </NavLink>
-        <NavDropdown label="Previous Years">
-          <NavLink href="#!">
-            2020 
+    <Box
+      as="header"
+      sx={{
+        backgroundColor: "backgroundTransparent",
+        backdropFilter: 'blur(20px)',
+        WebkitBackdropFilter: 'blur(20px)',
+        borderBottom: "1px solid",
+        borderBottomColor: "border",
+        position: "fixed",
+        left: 0,
+        right: 0,
+        zIndex: 999,
+      }}
+    >
+      <Flex sx={{maxWidth: "layout", mx: "auto", alignItems: "center"}} {...props}>
+        <Box sx={{flex: '1 1 auto', justifyContent: "", margin: '3'}}>
+          <NavLink href="#!" p={2}>
+            Register
           </NavLink>
-          <NavLink href="#!">
-            2019
+          <NavLink href="#!" p={2}>
+            About
           </NavLink>
-          <NavLink href="#!">
-            2018 
+          <NavLink href="#!" p={2}>
+            FAQ
           </NavLink>
-          <NavLink href="#!">
-            2017
+          <NavLink href="#!" p={2}>
+            Schedule
           </NavLink>
-        </NavDropdown>
-      </Box>
-      <ColorSwitcher />
-    </Flex>
+          <NavDropdown label="Previous Years">
+            <NavLink href="#!">
+              2020 
+            </NavLink>
+            <NavLink href="#!">
+              2019
+            </NavLink>
+            <NavLink href="#!">
+              2018 
+            </NavLink>
+            <NavLink href="#!">
+              2017
+            </NavLink>
+          </NavDropdown>
+        </Box>
+        <ColorSwitcher mr={3}/>
+      </Flex>
+    </Box>
   )
 }
 
