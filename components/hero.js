5d8dec1443eb5577add31076185615d98c277f42
googol88 <ronaldfleung@gmail.com>
2022-02-05 17:45:59 -0500

    🎨 chore: add new logo

--- components/hero.js
+++ components/hero.js
@@ -14,35 +14,22 @@ const Hero = props => (
       backgroundPosition: 'center bottom, left top',
       backgroundRepeat: 'repeat-x, repeat',
       backgroundSize: '600px, auto, auto',
-      /*filter: "drop-shadow(0px 0px 16px rgba(255, 214, 0, .1))",
-      WebkitFilter: "drop-shadow(0px 0px 16px rgba(255, 214, 0, .1))",*/
-      /*style='filter: drop-shadow(0px 0px 16px rgba(255, 214, 0, .1)); WebkitFilter: drop-shadow(0px 0px 16px rgba(255, 214, 0, .1))'*/
     }}
     {...props}
   >
-    {/*<Image
-      src="/mountains.svg"
-      alt="hillsHacks Logo"
-      sx={{
-        position: 'absolute',
-        width: ['300px', '400px', '500px', '600px'],
-        
-        userDrag: 'none',
-        WebkitUserDrag: 'none'
-      }}
-    />*/}
     <Container as="section" py={5} variant="copy" sx={{position: 'relative'}} {...props}>
       <Image
-        src="/logo.png"
+        src="/logo-v.png"
         alt="hillsHacks Logo"
         width={600}
         sx={{
           userDrag: 'none',
-          WebkitUserDrag: 'none'
+          WebkitUserDrag: 'none',
+          marginBottom: '-64px',
         }}
       />
-      <Heading mt={16} variant="subtitle" color="white"><b>Reaching New Heights</b></Heading>
-      <Heading variant="subtitle" color="white">Spring 2022</Heading>
+      <Heading mt={16} variant="subtitle" color="white" sx={{textShadow: "0 0 16px #000"}}><b>Reaching New Heights</b></Heading>
+      <Heading variant="subtitle" color="white" sx={{textShadow: "0 0 16px #000"}}>Spring 2022</Heading>
       <Button variant="lg" as="a" target="_blank" my={2}
     href="https://docs.google.com/forms/d/e/1FAIpQLSeEqpDhRwbn8WD1EXS8bAplyi_nqHJ0NdwdCSXQEPzcR5UYvQ/viewform?usp=sf_link">Pre-Register Now!</Button>
     </Container>
